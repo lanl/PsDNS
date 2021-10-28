7cc5d6b7b0e942ba2e2c2afb64c922207f68700b
Daniel M. Israel <dmi1@lanl.gov>
2021-10-28 11:44:00 -0600

    Checkpointing methods now return self.

--- psdns/bases.py
+++ psdns/bases.py
@@ -528,7 +528,8 @@ class SpectralArray(numpy.lib.mixins.NDArrayOperatorsMixin):
         fh.Write_all(self._data)
         fh.Close()
         view.Free()
-
+        return self
+    
     def read_checkpoint(self, filename):
         """Read a checkpoint file
 
@@ -541,6 +542,7 @@ class SpectralArray(numpy.lib.mixins.NDArrayOperatorsMixin):
         fh.Read_all(self._data)
         fh.Close()
         view.Free()
+        return self
 
     def copy(self):
         """Return a copy of the array"""
