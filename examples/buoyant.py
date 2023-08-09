Fixed sign and some other errors.
* There was a sign error in the Boussinesq class.
* Results for serial and parallel jobs are not consistent for certain versions of MPI.  The reason is still not clear, but the hunt did lead to some minor code fixes.
* Added a filename argument to FieldDump.

examples/buoyant.py
@@ -5,6 +5,7 @@ single mode Rayleigh-Taylor simulation.  The initial disturbance is a
 velocity disturbance that roughly corresponds to the linear
 instability eigenfunction.
 """
+import numpy
 from psdns import *
 from psdns.equations.navier_stokes import NavierStokes
 import scipy
@@ -49,19 +50,20 @@ class Boussinesq(NavierStokes):
             & = \left( \frac{k_{i}k_{j}}{k^{2}} - \delta_{ij} \right)
             \left( \widehat{u_{k}u_{j,k}} - \hat{c} g_{j} \right)
         """
+        g = -1
         u = uhat[:3].to_physical()
         vorticity = uhat[:3].curl().to_physical()
         nl = numpy.cross(u, vorticity, axis=0)
         nl = PhysicalArray(uhat[:3].grid, nl).to_spectral()
-        nl[2] += uhat[3]
+        nl[2] += g*uhat[3]
         du = numpy.einsum("ij...,j...->i...", uhat[:3].grid.P, nl)
         du -= self.nu*uhat.grid.k2*uhat[:3]
 
         gradc = uhat[3].grad().to_physical()
-        dc = numpy.einsum("i...,i...", u, gradc)
+        dc = - numpy.einsum("i...,i...", u, gradc)
         dc = PhysicalArray(uhat.grid, dc).to_spectral()
         dc -= self.Sc*self.nu*uhat.grid.k2*uhat[3]
-        
+
         return numpy.concatenate((du, dc[numpy.newaxis, ...]))
 
     def ic(self, grid):
@@ -71,18 +73,14 @@ class Boussinesq(NavierStokes):
         A = 0.1
         k = 1
         kx = ky = 1
-        u[0] = -A/2*k/kx*numpy.sign(eta)*numpy.exp(-numpy.abs(k*eta))*numpy.cos(kx*x[0])*numpy.sin(ky*x[1])
-        u[1] = -A/2*k/ky*numpy.sign(eta)*numpy.exp(-numpy.abs(k*eta))*numpy.sin(kx*x[0])*numpy.cos(ky*x[1])
-        u[2] = A*numpy.exp(-numpy.abs(k*eta))*numpy.sin(kx*x[0])*numpy.sin(ky*x[1])
-        u[3] = scipy.special.erf(eta/0.1)
+        u[3] = scipy.special.erf(eta/0.1+0.1*numpy.cos(x[0])*numpy.cos(x[1]))
         s = u.to_spectral()
         s._data = numpy.ascontiguousarray(s._data)
         return s
 
-
 grid = SpectralGrid(
-    sdims=[2**5-1, 2**5-1, 2**6-1],
-    pdims=[3*2**4, 3*2**4, 3*2**5],
+    sdims=[2**5-1, 2**5-1, 2**7-1],
+    pdims=[3*2**4, 3*2**4, 3*2**6],
     box_size=[2*numpy.pi, 2*numpy.pi, 8*numpy.pi]
     )
 equations = Boussinesq(Re=400)
@@ -93,8 +91,9 @@ solver = RungeKutta(
     equations=equations,
     ic=equations.ic(grid),
     diagnostics=[
-        StandardDiagnostics(tdump=0.1, grid=grid, fields=['tke', 'dissipation', 'divU'], outfile="tgv.dat"),
-        VTKDump(tdump=0.1, names=['U', 'V', 'W', 'C'], grid=grid)
+        FieldDump(tdump=1.0, grid=grid, filename="data{:04g}"),
+        StandardDiagnostics(tdump=0.1, grid=grid, fields=['tke', 'dissipation', 'divU'], outfile="std.dat"),
+        Profiles(tdump=0.1, grid=grid, outfile='profiles.dat'),
         ],
     )
 solver.run()