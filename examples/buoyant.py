ec991563252855d523f3551ad224bae5e5da78c8
Daniel M. Israel <dmi1@lanl.gov>
2023-10-24 13:46:02 -0600

    Updated bouyant class.
    * Moved the Boussinesq class from the example file to the navier_stokes module, and update the NavierStokes class so the two classes share as much code as possible.  In doing so, added passive scalars to the NavierStokes class.

--- examples/buoyant.py
+++ examples/buoyant.py
@@ -7,76 +7,9 @@ instability eigenfunction.
 """
 import numpy
 from psdns import *
-from psdns.equations.navier_stokes import NavierStokes
+from psdns.equations.navier_stokes import Boussinesq
 import scipy
 
-class Boussinesq(NavierStokes):
-    """The Navier-Stokes equation with Boussinesq approximation for buoyancy.
-
-    The incompressible Navier-Stokes equations, with a scalar
-    transport equation and the Boussinesq approximation for buoyancy
-    are:
-
-    .. math::
-
-        u_{i,i} & = 0 \\
-        u_{i,t} + u_{j} u_{i,j} 
-        & = -p_{,i} + \frac{1}{\text{Re}} u_{i,jj} +cg_{i} \\
-        c_{,t} + u_{j}c_{,j} & = \frac{\text{Sc}}{\text{Re}} c_{,jj}
-    """
-    def __init__(self, Sc=1, **kwargs):
-        """Return a Boussinesq equation object.
-
-        Intantiating returns a Boussinsesq equation object with
-        Reynolds number *Re* and Schmidt number *Sc*.
-        """
-        super().__init__(**kwargs)
-
-        self.Sc = Sc
-
-    def rhs(self, uhat):
-        r"""Compute the Boussinesq right hand side.
-
-        The numerical implementation is the same as for
-        :meth:`psdns.equations.navier_stokes.NavierStokes.rhs` execpt
-        for the additional bouyancy term.  As a result the momentum
-        equation becomes
-
-        .. math::
-
-            \left(
-              \frac{\partial}{\partial t} + \nu k^{2}
-            \right) \hat{u}_{i}
-            & = \left( \frac{k_{i}k_{j}}{k^{2}} - \delta_{ij} \right)
-            \left( \widehat{u_{k}u_{j,k}} - \hat{c} g_{j} \right)
-        """
-        g = -1
-        u = uhat[:3].to_physical()
-        vorticity = uhat[:3].curl().to_physical()
-        nl = numpy.cross(u, vorticity, axis=0)
-        nl = PhysicalArray(uhat[:3].grid, nl).to_spectral()
-        nl[2] += g*uhat[3]
-        du = numpy.einsum("ij...,j...->i...", uhat[:3].grid.P, nl)
-        du -= self.nu*uhat.grid.k2*uhat[:3]
-
-        gradc = uhat[3].grad().to_physical()
-        dc = - numpy.einsum("i...,i...", u, gradc)
-        dc = PhysicalArray(uhat.grid, dc).to_spectral()
-        dc -= self.Sc*self.nu*uhat.grid.k2*uhat[3]
-
-        return numpy.concatenate((du, dc[numpy.newaxis, ...]))
-
-    def ic(self, grid):
-        u = PhysicalArray(grid, (4,))
-        x = u.grid.x
-        eta = x[2] - 4*numpy.pi
-        A = 0.1
-        k = 1
-        kx = ky = 1
-        u[3] = scipy.special.erf(eta/0.1+0.1*numpy.cos(x[0])*numpy.cos(x[1]))
-        s = u.to_spectral()
-        s._data = numpy.ascontiguousarray(s._data)
-        return s
 
 grid = SpectralGrid(
     sdims=[2**5-1, 2**5-1, 2**7-1],
@@ -91,9 +24,9 @@ solver = RungeKutta(
     equations=equations,
     ic=equations.ic(grid),
     diagnostics=[
-        FieldDump(tdump=1.0, grid=grid, filename="data{:04g}"),
-        StandardDiagnostics(tdump=0.1, grid=grid, fields=['tke', 'dissipation', 'divU'], outfile="std.dat"),
-        Profiles(tdump=0.1, grid=grid, outfile='profiles.dat'),
+        FieldDump(tdump=1.0, grid=grid, filename="datab{:04g}"),
+        StandardDiagnostics(tdump=0.1, grid=grid, fields=['tke', 'dissipation', 'divU'], outfile="stdb.dat"),
+        Profiles(tdump=0.1, grid=grid, outfile='profilesb.dat'),
         ],
     )
 solver.run()
--- psdns/equations/navier_stokes.py
+++ psdns/equations/navier_stokes.py
@@ -17,7 +17,7 @@ class NavierStokes(object):
         u_{i,t} + u_j u_{i,j} & = - p_{,i} + \frac{1}{\mathrm{Re}} u_{i,jj}
 
     """
-    def __init__(self, Re, **kwargs):
+    def __init__(self, Re, Sc=1, **kwargs):
         """Return a Navier-Stokes equation object.
 
         Instantiating returns a Navier-Stokes equation object with
@@ -27,6 +27,7 @@ class NavierStokes(object):
 
         self.Re = Re
         self.nu = 1/Re
+        self.Sc = Sc
 
     def rhs(self, uhat):
         r"""Compute the Navier-Stokes right-hand side
@@ -87,14 +88,24 @@ class NavierStokes(object):
             = \left( \delta_{ij} - \frac{k_i k_j}{k^2} \right)
             \widehat{\left( \boldsymbol{u} \times \boldsymbol{\omega}\right)_j}
         """
-        u = uhat.to_physical()
-        vorticity = uhat.curl().to_physical()
+        u = uhat[:3].to_physical()
+        nl = self.nonlinear(uhat, u)
+        du = numpy.einsum("ij...,j...->i...", uhat.grid.P, nl)
+        du -= self.nu*uhat.grid.k2*uhat[:3]
+
+        gradc = uhat[3:].grad().to_physical()
+        dc = - numpy.einsum("j...,ij...->i...", u, gradc)
+        dc = PhysicalArray(uhat.grid, dc).to_spectral()
+        dc -= self.Sc*self.nu*uhat.grid.k2*uhat[3:]
+        
+        return numpy.concatenate((du, dc))
+
+    def nonlinear(self, uhat, u):
+        vorticity = uhat[:3].curl().to_physical()
         nl = numpy.cross(u, vorticity, axis=0)
         nl = PhysicalArray(uhat.grid, nl).to_spectral()
-        du = numpy.einsum("ij...,j...->i...", uhat.grid.P, nl)
-        du -= self.nu*uhat.grid.k2*uhat
-        return du
-
+        return nl
+    
     def taylor_green_vortex(self, grid, A=1, B=-1, C=0, a=1, b=1, c=1):
         r"""Initialize with the Taylor-Green problem
 
@@ -192,6 +203,65 @@ class NavierStokes(object):
         u[:,0,0,0] = 0
 
         return u
+
+class Boussinesq(NavierStokes):
+    """The Navier-Stokes equation with Boussinesq approximation for buoyancy.
+
+    The incompressible Navier-Stokes equations, with a scalar
+    transport equation and the Boussinesq approximation for buoyancy
+    are:
+
+    .. math::
+
+        u_{i,i} & = 0 \\
+        u_{i,t} + u_{j} u_{i,j} 
+        & = -p_{,i} + \frac{1}{\text{Re}} u_{i,jj} +cg_{i} \\
+        c_{,t} + u_{j}c_{,j} & = \frac{\text{Sc}}{\text{Re}} c_{,jj}
+    """
+    def __init__(self, g=-1, **kwargs):
+        """Return a Boussinesq equation object.
+
+        Intantiating returns a Boussinsesq equation object with
+        Reynolds number *Re* and Schmidt number *Sc*.
+        """
+        super().__init__(**kwargs)
+
+        self.g = g
+
+    def nonlinear(self, uhat, u):
+        r"""Compute the Boussinesq non-linear term.
+
+        The numerical implementation is the same as for
+        :meth:`psdns.equations.navier_stokes.NavierStokes.rhs` execpt
+        for the additional bouyancy term.  As a result the momentum
+        equation becomes
+
+        .. math::
+
+            \left(
+              \frac{\partial}{\partial t} + \nu k^{2}
+            \right) \hat{u}_{i}
+            & = \left( \frac{k_{i}k_{j}}{k^{2}} - \delta_{ij} \right)
+            \left( \widehat{u_{k}u_{j,k}} - \hat{c} g_{j} \right)
+        """
+        vorticity = uhat[:3].curl().to_physical()
+        nl = numpy.cross(u, vorticity, axis=0)
+        nl = PhysicalArray(uhat.grid, nl).to_spectral()
+        nl[2] += self.g*uhat[3]
+        return nl
+    
+    def ic(self, grid):
+        u = PhysicalArray(grid, (4,))
+        x = u.grid.x
+        eta = x[2] - 4*numpy.pi
+        A = 0.1
+        k = 1
+        kx = ky = 1
+        u[3] = scipy.special.erf(eta/0.1+0.1*numpy.cos(x[0])*numpy.cos(x[1]))-scipy.special.erf(x[2]/0.1)-scipy.special.erf((x[2]-8*numpy.pi)/0.1)
+        s = u.to_spectral()
+        s._data = numpy.ascontiguousarray(s._data)
+        return s
+
 
 class SimplifiedSmagorinsky(NavierStokes):
     r"""A simplified Smagorinsky-type model.
