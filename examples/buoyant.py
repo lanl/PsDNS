3a9cf566ab9f71fc9940bd0485a475c5fe4e6ebb
Daniel M. Israel <dmi1@lanl.gov>
2023-11-01 16:41:57 -0600

    Updated examples/buoyant.py.

--- examples/buoyant.py
+++ examples/buoyant.py
@@ -13,27 +13,31 @@ from psdns.equations.navier_stokes import Boussinesq
 grid = SpectralGrid(
     sdims=[2**5-1, 2**5-1, 2**7-1],
     pdims=[3*2**4, 3*2**4, 3*2**6],
-    box_size=[2*numpy.pi, 2*numpy.pi, 8*numpy.pi]
+    box_size=[4*numpy.pi, 4*numpy.pi, 16*numpy.pi]
     )
-equations = Boussinesq(Re=400)
+equations = Boussinesq(Re=100)
 
 x = grid.x[:2,:,:,0]
 solver = RungeKutta(
     dt=0.01,
-    tfinal=10.0,
+    tfinal=20.0,
     equations=equations,
     ic=equations.perturbed_interface(
         grid,
-        1e-6*equations.band(grid, 1, 4)
-        + 0.01*numpy.cos(2*numpy.pi*(2*x[0]/grid.box_size[0]))
-        * numpy.cos(2*numpy.pi*(2*x[1]/grid.box_size[1])),
+        1e-4*equations.band(grid, 1, 4)
+        + 0.01*numpy.cos(x[0]) * numpy.cos(x[1]),
+        numpy.sqrt(2*numpy.pi),
         0.1,
-        0.1
         ),
     diagnostics=[
         FieldDump(tdump=1.0, grid=grid, filename="data{:04g}"),
-        StandardDiagnostics(tdump=0.1, grid=grid, fields=['tke', 'dissipation', 'cavg', 'divU'], outfile="std.dat"),
+        StandardDiagnostics(
+            tdump=0.1, grid=grid,
+            fields=['tke', 'dissipation', 'cavg', 'divU'],
+            outfile="std.dat"
+            ),
         Profiles(tdump=0.1, grid=grid, outfile='profiles.dat'),
+        DissipationProfiles(tdump=0.1, grid=grid, outfile='dissipation.dat'),
         PressureProfiles(tdump=0.1, grid=grid, outfile='pressure.dat'),
         RapidPressProfiles(tdump=0.1, grid=grid, outfile='press_r.dat'),
         SlowPressProfiles(tdump=0.1, grid=grid, outfile='press_s.dat'),
